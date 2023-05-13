6d85bcea647a36b312dd799a17e27ac301f34c05
Ntsama Yann <tabetsingyannf@gmail.com>
2023-05-13 13:45:13 +0100

    added some command endpoints

--- backend/src/main/java/com/genielo/cafe/backend/controller/ItemController.java
+++ backend/src/main/java/com/genielo/cafe/backend/controller/ItemController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.genielo.cafe.backend.model.*;
 import com.genielo.cafe.backend.repository.*;
@@ -19,7 +16,6 @@ public class ItemController {
 
 	@Autowired
 	private ItemRepository itemRepository;
-	private CommandRepository commandRepository;
 	
 	@GetMapping("/items/breakfast")
 	public String getAllBreakfastItems(Model model){
@@ -50,25 +46,5 @@ public class ItemController {
 	    return "allItems";
 	}
 	    
-	@PostMapping("/commands/{command_id}/addItem/{item_id}")
-	public String makeNewCommand(Model model, @PathVariable("command_id") Long command_id, @PathVariable("item_id") Long item_id, RedirectAttributes redirectAttributes) {
-		try {
-			
-			Item item = itemRepository.findById(item_id).get();
-			List<Item> commandItems = new ArrayList<Item>();
-			commandItems.add(item);
-			Command command = commandRepository.findById(command_id).get();
-			command.setItems(commandItems);
-			commandRepository.save(command);
-			
-		} catch (Exception e) {
-			
-			redirectAttributes.addAttribute("message", e.getMessage());
-		}
-		return "redirect:/items";
-	
-	 
-  }
-	
-	
+		
 }
